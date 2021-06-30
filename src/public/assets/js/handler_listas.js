1232d389727341b3855d2f46207b078ab49957e5
papaganesha <32704567+papaganesha@users.noreply.github.com>
2021-06-30 01:41:39 -0300

    teste

--- src/public/assets/js/handler_listas.js
+++ src/public/assets/js/handler_listas.js
@@ -1,7 +1,6 @@
 import {timeInterval_20secs, timeInterval_3secs, timeOut_global, timeInterval_global, dismissable_warning_Msg, dismissable_sucess_Msg} from './common.js';
 
 
-
 window.onload = function(){
 if (localStorage.cod_usuario != 0 && localStorage.cod_usuario && localStorage.cod_usuario != null) {
     document.getElementById("busca").innerHTML = localStorage.nome;
